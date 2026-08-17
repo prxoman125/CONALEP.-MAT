10a2be500ea6ba25131cce5cc164cc5806fb4d11
prxoman125 <gjeremygonzalez105@gmail.com>
2026-08-16 20:26:18 -0600

    Update Formula.py

--- Formula.py
+++ Formula.py
@@ -28,7 +28,7 @@ st.markdown(f"""
     
     .logo-conalep-flotante{{
         position: fixed;
-        top: 50px;
+        top: 40px;
         right: 15px;
         z-index: 9999999;
         width: 100px;
